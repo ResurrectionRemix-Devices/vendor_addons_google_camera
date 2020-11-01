0fce39de1e3549ff942a736928daac9abd7fd588
AnierinB <anierin.t.bliss@gmail.com>
2020-10-31 21:28:00 -0700

    google_camera: Update to 7.3.018 Urnyx05 v2.4
    Signed-off-by: AnierinB <anierin.t.bliss@gmail.com>

--- config.mk
+++ config.mk
@@ -3,4 +3,4 @@ PRODUCT_PACKAGES += \
     GoogleCamera
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml
+    $(LOCAL_PATH)/system/etc/permissions/com.google.android.GoogleCamera.Urnyx.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.Urnyx.xml
